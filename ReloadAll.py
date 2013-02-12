a1d3be03ce8918371aa877d5681f1c3578970098
Robbert <robbertbloem@gmail.com>
2013-02-12 08:13:59 +0100

    new module reloadall

---
+++ ReloadAll.py
@@ -0,0 +1,29 @@
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import types
+from imp import reload
+
+def status(module):
+    print("reloading " + module.__name__)
+
+def transitive_reload(module, visited):
+    if not module in visited:
+        status(module)
+        reload(module)
+        visited[module] = None
+        for attrobj in module.__dict__.values():
+            if type(attrobj) == types.ModuleType:
+                transitive_reload(attrobj, visited)
+
+def reload_all(*args):
+    visited = {}
+    for arg in args:
+        if type(arg) == types.ModuleType:
+            transitive_reload(arg, visited)
+
+if __name__ == "__main__":
+    import ReloadAll
+    reload_all(ReloadAll)
