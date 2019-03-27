f6dbea43ec44d73824fdb2466ab8ae24161566ff
Robbert <github_commit@robbert.org>
2019-03-27 20:25:06 +0100

    implemented make_path_and_filename
    including tests

--- Tests/CommonFunctions_tests.py
+++ Tests/CommonFunctions_tests.py
@@ -9,28 +9,63 @@ import numpy
 import matplotlib 
 import matplotlib.pyplot as plt
 
-import PythonTools as PT
+import PythonTools.CommonFunctions as CF
 
-importlib.reload(PT)
+importlib.reload(CF)
 
 
-class Test_save_data(unittest.TestCase):
+class Test_make_path_and_filename(unittest.TestCase):
 
     def setUp(self):
         self.verbose = 1
-        self.path = pathlib.Path("Testdata\save_data")
         
+
+        
+#         print(type(self.path_str))
+        
+        
+                
+        
+    def test_simple(self):
+    
+        path = pathlib.Path(__file__).parent
+         
+        p_pl = path.joinpath(pathlib.Path("Testdata"))
+        p_str = str(p_pl)
+        
+        f_pl = pathlib.Path("test_file.dat")
+        f_str = str(f_pl)
+        
+        tests = [
+# path and filename are pathlib or string, output as pathlib
+{"path": p_pl, "filename": f_pl, "extension": None, "string_out": False},
+{"path": p_str, "filename": f_pl, "extension": None, "string_out": False},
+{"path": p_pl, "filename": f_str, "extension": None, "string_out": False},
+{"path": p_str, "filename": f_str, "extension": None, "string_out": False},
+# path and filename are pathlib or string, output as string
+{"path": p_pl, "filename": f_pl, "extension": None, "string_out": True},
+{"path": p_str, "filename": f_pl, "extension": None, "string_out": True},
+{"path": p_pl, "filename": f_str, "extension": None, "string_out": True},
+{"path": p_str, "filename": f_str, "extension": None, "string_out": True},
+# path and filename are pathlib or string, output as string
+{"path": p_pl, "filename": f_pl, "extension": "dat", "string_out": True},
+{"path": p_pl, "filename": f_pl, "extension": "csv", "string_out": True},
+{"path": p_pl, "filename": f_pl, "extension": pathlib.Path(".csv"), "string_out": True},
+        ]
+        
+        
+        for t in tests:
+            paf = CF.make_path_and_filename(path = t["path"], filename = t["filename"], extension = t["extension"], string_out = t["string_out"])
+            self.assertTrue((type(paf) == str) == t["string_out"])
+            print(paf)
 
 
         
         
 if __name__ == '__main__': 
-    verbosity = 2
+    verbosity = 1
     
     
     if 1:
-        """
-        + __init__
-        """
-        suite = unittest.TestLoader().loadTestsFromTestCase(Test_init)
+        suite = unittest.TestLoader().loadTestsFromTestCase( Test_make_path_and_filename)
         unittest.TextTestRunner(verbosity=verbosity).run(suite)
